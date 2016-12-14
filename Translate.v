4867d6526908ffddd65e49cc7aafcb0eda67b106
Yishuai Li <yishuai@cis.upenn.edu>
2016-12-13 21:31:32 -0500

    kprog

--- Translate.v
+++ Translate.v
@@ -23,14 +23,14 @@ Inductive kexp : e -> e -> e -> Prop :=
          (e_ann (u_app k (e_ann (u_int n)
                                 (ktype ty)))
                 t_void)
-| exp_lam : forall ut t1 e2 u2 t2 x1 c1 e' k,
+| exp_lam : forall ut t1 e2 u2 t2 e' k,
     ut = u_lam t1 e2 ->
     e2 = (e_ann u2 t2) ->
-    x1 `notin` (fv_u ut) ->
+    (forall x1 c1, x1 `notin` (fv_u ut) ->
     c1 `notin` (fv_u ut) ->
         let xt := u_var_f x1 in
         let ct := u_var_f c1 in
-        kexp (open_e_wrt_u e2 xt) (e_ann ct (kcont t2)) (open_e_wrt_u (open_e_wrt_u e' xt) ct) ->
+        kexp (open_e_wrt_u e2 xt) (e_ann ct (kcont t2)) (open_e_wrt_u (open_e_wrt_u e' xt) ct)) ->
     kexp (e_ann ut (t_arr t1 t2)) k
          (e_ann (u_app k
                        (e_ann
@@ -40,15 +40,15 @@ Inductive kexp : e -> e -> e -> Prop :=
                                         (kcont t2)))
                           (kcont (t_arr t1 t2))))
                 t_void)
-| exp_app : forall e1 e1' u1 t1 e2 e2' u2 t2 c1 c2 ty k,
+| exp_app : forall e1 e1' u1 t1 e2 e2' u2 t2 ty k,
     e1 = e_ann u1 t1 ->
     e2 = e_ann u2 t2 ->
-    c1 `notin` (fv_e e1) ->
-    c2 `notin` (fv_e e2) ->
+    (forall c1, c1 `notin` (fv_e e1) ->
     let ct := u_var_f c1 in
-    kexp e1 (e_ann ct (kcont t1)) (open_e_wrt_u e1' ct) ->
+    kexp e1 (e_ann ct (kcont t1)) (open_e_wrt_u e1' ct)) ->
+    (forall c2, c2 `notin` (fv_e e2) ->
     let ct := u_var_f c2 in
-    kexp e2 (e_ann ct (kcont t2)) (open_e_wrt_u e2' ct) ->
+    kexp e2 (e_ann ct (kcont t2)) (open_e_wrt_u e2' ct)) ->
     kexp (e_ann (u_app e1 e2) ty) k
          (open_e_wrt_u e1'
                        (u_lam (ktype t1)
@@ -60,13 +60,13 @@ Inductive kexp : e -> e -> e -> Prop :=
                                                                                       (ktype t2)))
                                                                         (t_arr (kcont t2) t_void)) k)
                                                           t_void)))))
-| exp_prim : forall e1 e1' e2 e2' c1 c2 p ty k,
-    c1 `notin` (fv_e e1) ->
-    c2 `notin` (fv_e e2) ->
+| exp_prim : forall e1 e1' e2 e2' p ty k,
+    (forall c1, c1 `notin` (fv_e e1) ->
     let ct := u_var_f c1 in
-    kexp e1 (e_ann ct (kcont t_int)) (open_e_wrt_u e1' ct) ->
+    kexp e1 (e_ann ct (kcont t_int)) (open_e_wrt_u e1' ct)) ->
+    (forall c2, c2 `notin` (fv_e e2) ->
     let ct := u_var_f c2 in
-    kexp e2 (e_ann ct (kcont t_int)) (open_e_wrt_u e2' ct) ->
+    kexp e2 (e_ann ct (kcont t_int)) (open_e_wrt_u e2' ct)) ->
     kexp (e_ann (u_prim e1 p e2) ty) k
          (open_e_wrt_u e1'
                        (u_lam t_int
@@ -77,10 +77,10 @@ Inductive kexp : e -> e -> e -> Prop :=
                                                                         t_int)
                                                                  (u_app k (e_ann (u_var_b 0) t_int)))
                                                           t_void)))))
-| exp_if0 : forall e1 e1' e2 e2' e3 e3' c1 ty k,
-    c1 `notin` (fv_e e1) ->
+| exp_if0 : forall e1 e1' e2 e2' e3 e3' ty k,
+    (forall c1, c1 `notin` (fv_e e1) ->
     let ct := u_var_f c1 in
-    kexp e1 (e_ann ct (kcont t_int)) (open_e_wrt_u e1' ct) ->
+    kexp e1 (e_ann ct (kcont t_int)) (open_e_wrt_u e1' ct)) ->
     kexp e2 k e2' ->
     kexp e3 k e3' ->
     kexp (e_ann (u_if0 e1 e2 e3) ty) k
@@ -91,3 +91,17 @@ Inductive kexp : e -> e -> e -> Prop :=
                                             e3')
                                      t_void)))
 .
+
+Inductive kprog : e -> e -> Prop :=
+| prog_halt:
+    forall e1 u1 ty e',
+    e1 = e_ann u1 ty ->
+    (forall c1,
+        c1 `notin` (fv_e e1) ->
+        let ct := u_var_f c1 in
+        kexp e1 (e_ann ct (kcont ty)) (open_e_wrt_u e' ct)) ->
+    kprog e1
+          (open_e_wrt_u e'
+                        (u_lam (ktype ty)
+                               (e_ann (u_halt (e_ann (u_var_b 0) (ktype ty)))
+                                      t_void))).
